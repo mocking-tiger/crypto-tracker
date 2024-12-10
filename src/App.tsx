6e2d555d530446c915e837e17224ac0f338037a7
Park JunYong <dyden119@gmail.com>
2024-12-10 21:44:24 +0900

    docs: 타입스크립트 설치

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import "./App.css";
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
